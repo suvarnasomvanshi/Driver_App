57edfed15a8d0d030f6232c407f5ce655b606deb
suvarnasomvanshi <119663386+suvarnasomvanshi@users.noreply.github.com>
2024-06-04 22:29:25 +0530

    Update firebase.config.js

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -3,14 +3,14 @@ import { initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAzj__sssj2pm6NeW1AkbKmPEpBmlYD3HEcXM",
-  authDomain: "drivershaab-1269bsss.firebaseapp.com",
-  databaseURL: "https://drivershaab-1269b-default-rtdb.firebaseio.com",
-  projectId: "drivershaab-1269bsss",
-  storageBucket: "drivershaab-1269sssb.appspot.com",
-  messagingSenderId: "52372438306098",
-  appId: "1:523724383060:web:e88b36ce092196b8141ec198",
-  measurementId: "G-55SKE69D1WSSS"
+  apiKey: "32784ggsdsssj2pm6NeW1AkbKmPEpBmlYD3HEcXM",
+  authDomain: "dssdsdsdbsss.firebaseapp.com",
+  databaseURL: "https:/edfdfewrewd1269b-default-rtdb.firebaseio.com",
+  projectId: "driversfewdsfss",
+  storageBucket: "drivefereew269sssb.appspot.com",
+  messagingSenderId: "5edfewdwerw306098",
+  appId: "1:523724383060:web:ewrwewe92196b8141ec198",
+  measurementId: "edfwerwd1WSSS"
 };
 
 // Initialize Firebase
